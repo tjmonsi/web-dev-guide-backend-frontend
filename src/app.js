3b13b6ec3aff16f438a28b96a7828209434de054
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2022-08-03 06:29:39 +0000

    chore: add a post todo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,46 @@
 import Fastify from 'fastify';
+import { v4 } from 'uuid';
+import { readFileSync, writeFileSync } from 'fs';
 
 export const build = async (opts = {}) => {
   const fastify = Fastify(opts);
 
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+  const prefix = '/api';
+  const filename = './db.json';
+
+  fastify.post(`${prefix}/todo`, async (request, reply) => {
+    const { body } = request;
+    const { todo } = body;
+
+    // read the "database"
+    const db = JSON.parse(readFileSync(filename, 'utf8'));
+
+    // create a new data entry
+    const id = v4();
+    db.todo[`${id}`] = {
+      todo,
+      createdDate: new Date().getTime()
+    };
+
+    // save to database
+    writeFileSync(filename, JSON.stringify(db, null, 2), 'utf8');
+
+    // return results
+    /**
+     * equal to:
+     * const data = {
+     *   id: id
+     * }
+     * 
+     * // iterate the keys
+     * for (const key in db.todo[`${id}`]) {
+     *   data[`${key}`] = db.todo[`${id}`][`${key}`]
+     * }
+     */
+    return {
+      id,
+      ...db.todo[`${id}`]
+    }
   });
 
   return fastify;
